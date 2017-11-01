15943e5d1703a0da12f6a94b20334b83a918e91f
iCode18 <alavi.afshin@gmail.com>
2017-11-01 08:51:53 +0100

    Added font and renamed some

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-from kivy.app import App
+from time import strftime
 
+from kivy.app import App
+from kivy.clock import Clock
+
+
+class ClockApp(App):
+    pass
+
+    def update_time(self, nap):
+        self.root.ids.time.text = strftime('[b] %H [/b]:%M:%S')
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 1)
+
+if __name__ == '__main__':
+    from kivy.core.text import LabelBase
+    from kivy.core.window import Window
+    from kivy.utils import get_color_from_hex
+
+    Window.clearcolor = get_color_from_hex('#101216')
+    LabelBase.register(name='Roboto',
+                       fn_regular='Roboto-Thin.ttf',
+                       fn_bold='Roboto-Medium.ttf')
+    ClockApp().run()
